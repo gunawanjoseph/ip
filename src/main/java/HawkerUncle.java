cecbe6809de2bc810f49fa73d4d642fcc67c7111
Joseph <josephgroups100@gmail.com>
2025-08-25 19:34:21 +0800

    Implement Echo Function for HawkerUncle.java

--- src/main/java/HawkerUncle.java
+++ src/main/java/HawkerUncle.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class HawkerUncle {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm HawkerUncle");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true){
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")){
+                System.out.println(" Bye. Hope to se you again soon!");
+                break;
+            }
+
+            System.out.println("  " + input);
+        }
+
+        scanner.close();
     }
 }
