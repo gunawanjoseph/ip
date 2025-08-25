253d2db2fd0d94b51204e6795c210284dae5916e
Joseph <josephgroups100@gmail.com>
2025-08-25 20:30:51 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/HawkerUncle.java
+++ src/main/java/HawkerUncle.java
@@ -3,18 +3,29 @@ import java.util.Scanner;
 public class HawkerUncle {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm HawkerUncle");
-        System.out.println("What can I do for you?");
+        System.out.println("  Hello! I'm HawkerUncle");
+        System.out.println("  What can I do for you?");
+
+        String[] texts = new String[100];
+        int textCount = 0;
 
         while(true){
             String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("bye")){
-                System.out.println(" Bye. Hope to se you again soon!");
+                System.out.println(" Bye. Hope to see you again soon!");
                 break;
             }
-
-            System.out.println("  " + input);
+            else if (input.equalsIgnoreCase("list")){
+                for (int i = 0; i < textCount; ++i){
+                    System.out.println("  " + (i + 1) + ". " + texts[i]);
+                }
+            }
+            else{
+                System.out.println("  added: " + input);
+                texts[textCount] = input;
+                textCount++;
+            }
         }
 
         scanner.close();
