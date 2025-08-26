d944bacd123fc3deaa959598dfeb79a0a1e21e10
Joseph <josephgroups100@gmail.com>
2025-08-26 11:11:45 +0800

    feat: Added Automated Testing setup for HawkerUncle

--- src/main/java/HawkerUncle.java
+++ src/main/java/HawkerUncle.java
@@ -13,7 +13,7 @@ public class HawkerUncle {
             String input = scanner.nextLine();
 
             if (input.equalsIgnoreCase("bye")){
-                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("  Bye. Hope to see you again soon!");
                 break;
             }
             else if (input.equalsIgnoreCase("list")){
